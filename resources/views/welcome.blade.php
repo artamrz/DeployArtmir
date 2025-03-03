bb80560820ef6d05abe917982e4e4bb06f143020
Arta <arta.mirzadeh@gmail.com>
2025-03-02 16:49:09 -0800

    posts pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,15 +37,19 @@
  <div class="container">
     <div class="p-5 mb-4 bg-light rounded-3">
               <div class="container-fluid py-5">
-                <h1 class="display-5 fw-bold">Why AI is the new solution?</h1>
-                <p class="col-md-8 fs-4">This is my first blog using Laravel version 8. Hope you find it intresting! please check my most popular posts!</p>
-                <button class="btn btn-dark btn-lg" type="button">Read More</button>
+                @foreach($posts as $post)
+                  <h1 class="display-5 fw-bold">{{ $post->title }}</h1>
+                  <p class="col-md-8 fs-4">{{ substr(strip_tags($post->body),0,270) }}{{strlen(strip_tags($post->body)) >300 ? "..." : ""}}</p>
+                  <a href="{{url('rlog/'.$post->slug)}}" class="btn btn-dark">Read More</a>
+                  <hr>
+                @endforeach  
               </div>
         </div>
     </div>   
 </div>
 @endsection
-  
+
+        
      
 
  
