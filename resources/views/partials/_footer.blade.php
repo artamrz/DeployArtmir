81415be05708a0edf00120bd5d83c4c9ba7c1fa6
Arta <arta.mirzadeh@gmail.com>
2025-03-03 11:35:12 -0800

    footer changes

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,47 +1,49 @@
    <div class="container-fluid bg-dark">
        <footer class="d-flex justify-content-center m-5">
-           <div class="col-sm-4 offset-sm-1">
+           <div class="row">
+               <div class="col-sm-4 offset-sm-1">
                <a href="/" class="d-flex align-items-center mt-3 mb-3 link-dark text-decoration-none">
                    <img src="{{ asset('img/Rtamir-logo-tps0-200px.png') }}" alt="RtaMirLogo" width="207" height="170">
                </a>
            </div>
            <div class="col-sm-3 offset-sm-3">
-               <h5 class="text-light">Services</h5>
+               <h6 class="text-light">Services</h6>
                <ul class="nav navbar-nav flex-column">
                    <li class="nav-item mb-2">
-                       <a href="/webapp" class="nav-link text-decoration-none link-light p-0">Web and Application Design</a>
+                       <a href="/webapp" class="nav-link text-decoration-none link-light link-secondary p-0">Web and Application Design</a>
                    </li>
                    <li class="nav-item mb-2">
-                      <a href="/seo" class="nav-link text-decoration-none link-light p-0">SEO and GEO Optimization</a> 
+                      <a href="/seo" class="nav-link text-decoration-none link-light link-secondary p-0">SEO and GEO Optimization</a> 
                    </li>    
                    <li class="nav-item mb-2">   
-                       <a href="/content" class="nav-link text-decoration-none link-light p-0">Content Management</a>
+                       <a href="/content" class="nav-link text-decoration-none link-secondary link-light p-0">Content Management</a>
                    </li>
                </ul>
            </div>
            <div class="col-sm-3">
-               <h5 class="text-light">Weblog</h5>
+               <h6 class="text-light">Weblog</h6>
                    <ul class="nav navbar-nav flex-column">
                        <li class="nav-item mb-2">
-                           <a href="/rlog" class="nav-link text-decoration-none link-light p-0">Rlog Posts</a>
+                           <a href="/rlog" class="nav-link text-decoration-none link-secondary link-light p-0">Rlog Posts</a>
                        </li>
                        <li class="nav-item mb-2">
-                           <a href="/about" class="nav-link text-decoration-none link-light p-0">RtaMir</a>
+                           <a href="/about" class="nav-link text-decoration-none link-secondary link-light p-0">RtaMir</a>
                        </li>
                </ul>
-               <h5 class="text-light">How To Connect</h5>
+               <h6 class="text-light">How To Connect</h6>
                <ul class="nav navbar-nav flex-column">
                    <li class="nav-item mb-2">
-                       <a href="/contact" class="nav-link text-decoration-none link-light p-0">Contact Form</a>
+                       <a href="/contact" class="nav-link text-decoration-none link-secondary link-light p-0">Contact Form</a>
                    </li>
                    <li class="nav-item mb-2">    
-                       <a href="#" class="nav-link text-decoration-none link-light p-0">Github | Linked-in | Email</a>
+                       <a href="#" class="nav-link text-decoration-none link-secondary link-light p-0">Github | Linked-in | Email</a>
                    </li>
                </ul>
            </div>
+           </div>
        </footer>
     </div>
-    <div class="container-fluid bg-dark d-flex border-top border-secondary">   
+    <div class="container-fluid bg-dark border-top border-secondary">   
        <div class="row justify-content-center m-5 text-muted mb-3 mt-3 ">Copyright © 2025 RtaMir| Innovating the Future | All Rights Reserved</div>
    </div>
    
