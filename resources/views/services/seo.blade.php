25532faa22239106a5e648ff5aab7ac5e8242835
Arta <arta.mirzadeh@gmail.com>
2025-03-03 15:24:20 -0800

    footer changes

---
+++ resources/views/services/seo.blade.php
@@ -0,0 +1,12 @@
+
+    @extends('main')
+    @section('title','SEO')
+    @section('content')
+       <div class="row">
+            @include('partisals._carousel')
+       </div>
+    @endsection
+    @section('scripts')
+
+    @endsection
+
