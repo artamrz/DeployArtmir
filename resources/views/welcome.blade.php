3cc83808d3df2e5699b38725c9801cd449079f92
Arta <arta.mirzadeh@gmail.com>
2025-03-03 14:53:44 -0800

    footer changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,20 +9,23 @@
    <div class="row">
      <div class="col-md-4 ">
          <img src="{{ asset('img/RD.jpg')}}" class="mx-auto d-block rounded-circle m-3" width="140" height="140" alt=""/>
-         <h2 class="fw-normal text-center">Context-Aware </h2>
-         <p class="text-center p-3">A responsive layout is no longer sufficient; dynamic adaptation of content and layout based on location, context, and connection speed must be prioritized.</p>
+         <h3 class="fw-normal text-center">Context-Aware </h3>
+         <p class="service text-center p-3">A responsive layout is no longer sufficient; dynamic adaptation of content and layout based on location, context, and connection speed must be prioritized.</p>
      </div>
      <div class="col-md-4">
        <img src="{{ asset('img/SEO.jpg')}}" class="mx-auto d-block rounded-circle m-3" width="140" height="140" alt=""/>
-       <h2 class="fw-normal text-center">SEO Optimization </h2>
-       <p class="text-center p-3">Implements strategies to improve rankings on search engines and AI-driven resources, boosting organic traffic and visibility.</p>
+       <h3 class="fw-normal text-center">SEO Optimization </h3>
+       <p class="service text-center p-3">Implements strategies to improve rankings on search engines and AI-driven resources, boosting organic traffic and visibility.</p>
      </div>
      <div class="col-md-4">
        <img src="{{ asset('img/CC1.jpg')}}" class="mx-auto d-block rounded-circle m-3" width="140" height="140" alt=""/>
-       <h2 class="fw-normal text-center"> Balanced Design</h2>
-       <p class="text-center p-3 mb-0">Web and software designs should be interactive yet simple, ensuring that neither aspect is compromised for the sake of the other.</p>
+       <h3 class="fw-normal text-center"> Balanced Design</h3>
+       <p class="service text-center p-3 mb-0">Web and software designs should be interactive yet simple, ensuring that neither aspect is compromised for the sake of the other.</p>
      </div>
    </div>
+ </div>
+ <div class="container-fluid bg-dark d-flex py-3 justify-content-center">
+ @include('partials._carousel')
  </div>
 <div class="container-fluid bg-dark d-flex py-3 justify-content-center">
     <div class="row w-100 justify-content-center align-items-center">
