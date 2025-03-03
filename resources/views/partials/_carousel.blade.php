bdeb47c7ad58760693c206f7e2160723c98e1a18
Arta <arta.mirzadeh@gmail.com>
2025-03-03 15:35:53 -0800

    carousel

--- resources/views/partials/_carousel.blade.php
+++ resources/views/partials/_carousel.blade.php
@@ -5,7 +5,7 @@
     <input type="radio" name="position" />
     <main id="carousel">
       <div class="item">
-        <img src="images/1.jpg" />
+        <img src="x{{ asset('img/1.jpg')}}" />
         <h3 class="name">Sally Sharpe</h3>
         <p class="occupation">Marketing Admin</p>
         <p class="testimonial">
@@ -14,7 +14,7 @@
         </p>
       </div>
       <div class="item">
-        <img src="images/2.jpg" />
+        <img src="{{ asset('img/2.jpg')}}" />
         <h3 class="name">Michael John</h3>
         <p class="occupation">Cybersecurity Engineer</p>
         <p class="testimonial">
@@ -23,7 +23,7 @@
         </p>
       </div>
       <div class="item">
-        <img src="images/3.jpg" />
+        <img src="{{ asset('img/3.jpg')}}" />
         <h3 class="name">Mikayla Eddie</h3>
         <p class="occupation">Software Engineer</p>
         <p class="testimonial">
@@ -32,7 +32,7 @@
         </p>
       </div>
       <div class="item">
-        <img src="images/4.jpg" />
+        <img src="{{ asset('img/4.jpg')}}" />
         <h3 class="name">Eve Smith</h3>
         <p class="occupation">UI/UX Designer</p>
         <p class="testimonial">
@@ -41,7 +41,7 @@
         </p>
       </div>
       <div class="item">
-        <img src="images/5.jpg" />
+        <img src="{{ asset('img/5.jpg')}}" />
         <h3 class="name">Luke Maxwell</h3>
         <p class="occupation">System Architect</p>
         <p class="testimonial">
@@ -50,3 +50,4 @@
         </p>
       </div>
     </main>
+
