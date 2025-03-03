6a57b311418746644fe9686679f1d96931d53cc9
Arta <arta.mirzadeh@gmail.com>
2025-03-03 15:33:02 -0800

    carousel

--- resources/views/partials/_carousel.blade.php
+++ resources/views/partials/_carousel.blade.php
@@ -1,76 +1,52 @@
-<div class="carousel-container">
-    <div class="carousel-wrapper">
-        <div class="carousel">
-          <div class="item">
-            <img src="{{ asset('img/1.jpg')}}" />
-            <h3>Sally Sharpe</h3>
-            <p>Marketing Admin</p>
-          </div>
-          <div class="item">
-            <img src="{{ asset('img/2.jpg')}}" />
-            <h3>Michael John</h3>
-            <p>Cybersecurity Engineer</p>
-          </div>
-          <div class="item">
-            <img src="{{ asset('img/3.jpg')}}" />
-            <h3>Mikayla Eddie</h3>
-            <p>Software Engineer</p>
-          </div>
-          <div class="item">
-            <img src="{{ asset('img/4.jpg')}}" />
-            <h3>Eve Smith</h3>
-            <p>UI/UX Designer</p>
-          </div>
-          <div class="item">
-            <img src="{{ asset('img/5.jpg')}}" />
-            <h3>Luke Maxwell</h3>
-            <p>System Architect</p>
-          </div>
-        </div>
-        <a class="carousel-control-prev nav-button" id="next" href="#myCarousel" role="button" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </a>
-        <a class="carousel-control-next nav-button" id="next" href="#myCarousel" role="button" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </a>
-    </div>
-</div>
-@section('scripts')
-    <script>
-      const items = document.querySelectorAll(".item");
-      let position = 2; // Default center position (0-based index)
-
-      function updateCarousel() {
-        items.forEach((item, index) => {
-          const offset = index - position;
-          const absOffset = Math.abs(offset);
-
-          item.style.transform = `rotateY(${offset * -15}deg) translateX(${
-            offset * -350
-          }px) scale(${absOffset === 0 ? 1 : 0.8})`;
-          item.style.zIndex = `${5 - absOffset}`;
-          item.style.opacity = absOffset > 2 ? "0.3" : "1";
-          item.style.transition =
-            "transform 0.5s ease-in-out, opacity 0.5s ease-in-out";
-        });
-      }
-
-      document.getElementById("prev").addEventListener("click", () => {
-        if (position > 0) {
-          position--;
-          updateCarousel();
-        }
-      });
-
-      document.getElementById("next").addEventListener("click", () => {
-        if (position < items.length - 1) {
-          position++;
-          updateCarousel();
-        }
-      });
-
-      updateCarousel();
-    </script>
-@endsection
+<input type="radio" name="position" />
+    <input type="radio" name="position" />
+    <input type="radio" name="position" checked />
+    <input type="radio" name="position" />
+    <input type="radio" name="position" />
+    <main id="carousel">
+      <div class="item">
+        <img src="images/1.jpg" />
+        <h3 class="name">Sally Sharpe</h3>
+        <p class="occupation">Marketing Admin</p>
+        <p class="testimonial">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto,
+          suscipit?
+        </p>
+      </div>
+      <div class="item">
+        <img src="images/2.jpg" />
+        <h3 class="name">Michael John</h3>
+        <p class="occupation">Cybersecurity Engineer</p>
+        <p class="testimonial">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
+          dolore.
+        </p>
+      </div>
+      <div class="item">
+        <img src="images/3.jpg" />
+        <h3 class="name">Mikayla Eddie</h3>
+        <p class="occupation">Software Engineer</p>
+        <p class="testimonial">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur,
+          temporibus?
+        </p>
+      </div>
+      <div class="item">
+        <img src="images/4.jpg" />
+        <h3 class="name">Eve Smith</h3>
+        <p class="occupation">UI/UX Designer</p>
+        <p class="testimonial">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod,
+          beatae?
+        </p>
+      </div>
+      <div class="item">
+        <img src="images/5.jpg" />
+        <h3 class="name">Luke Maxwell</h3>
+        <p class="occupation">System Architect</p>
+        <p class="testimonial">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse,
+          tempore.
+        </p>
+      </div>
+    </main>
