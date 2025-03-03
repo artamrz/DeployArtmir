2b14c82cbc82a7df816b05ee77a825f6d0c759cf
Arta <arta.mirzadeh@gmail.com>
2025-03-02 21:20:13 -0800

    posts pages

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,12 +1,47 @@
-   <div class="bg-dark">
-        <footer>
-            <ul class="nav justify-content-center border-bottom py-4">
-                    <li class="nav-item"> <a href="#" class="nav-link px-2 text-muted">Find Me On</a></li>
-                    <li class="nav-item"> <a href="#" class="nav-link px-2 text-muted">Github</a></li>
-                    <li class="nav-item"> <a href="#" class="nav-link px-2 text-muted">Linked-in</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link px-2 text-muted" >Youtube</a></li>
-            </ul>
-        </footer>
-        <p class="text-center text-light footer-background-dark mb-3 mt-3 ">Rta Mir Space - All Rights Reserved</p>
-   </div> 
+   <div class="container bg-dark">
+       <footer class="row row-cols-5 py-5 my-5 border-top">
+           <div class="col">
+               <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
+                   <img src="{{ asset('img/Rtamir-logo-tps0-200px.png') }}" alt="RtaMirLogo" width="107" height="70">
+               </a>
+           </div>
+           <div class="col"></div>
+           <div class="col">
+               <h5>Services</h5>
+               <ul class="nav flex-column">
+                   <li class="nav-item mb-2">
+                       <a href="{{url('services.webApp')}}" class="nav-link p-0">Web and Application Design</a>
+                   </li>
+                   <li>
+                      <a href="{{url('services.seo')}}" class="nav-link p-0">SEO and GEO Optimization</a> 
+                   </li>    
+                   <li>   
+                       <a href="{{url('services.content')}}" class="nav-link p-0">Content Management</a>
+                   </li>
+               </ul>
+           </div>
+           <div class="col">
+               <h5>Weblog</h5>
+                   <ul class="nav flex-column">
+                       <li class="nav-item mb-2">
+                           <a href="/rlog" class="nav-link p-0">Rlog Posts</a>
+                       </li>
+                       <li class="nav-item mb-2">
+                           <a href="/about" class="nav-link p-0">RtaMir</a>
+                       </li>
+               </ul>
+               <h5>How To Connect</h5>
+               <ul class="nav flex-column">
+                   <li class="nav-item mb-2">
+                       <a href="/contact" class="nav-link p-0">Contact Form</a>
+                   </li>
+                   <li>    
+                       <a href="#" class="nav-link p-0">Github | Linked-in | Email</a>
+                   </li>
+               </ul>
+           </div>
+       </footer>
+       <p class="text-center text-light footer-background-dark mb-3 mt-3 ">Copyright © 2025 RtaMir| Innovating the Future | All Rights Reserved</p>
+   </div>
+   
    
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\ServiceController;
+
 
 Route::get('/', [PageController::class,'getIndex']);
 
@@ -16,7 +18,9 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/about', [PageController::class,'getAbout']);
 Route::get('/contact', [PageController::class,'getContact']);
-
+Route::get('/webapp', [ServiceController::class,'getContact']);
+Route::get('/seo', [ServiceController::class,'getContact']);
+Route::get('/content', [ServiceController::class,'getContact']);
 
 Route::get('rlog/{slug}',[BlogController::class, 'getSingle']) ->name('rlog.single')->where('slug','[\w\d\-\_]+');
 Route::get('rlog',[BlogController::class, 'getIndex'])->name('rlog.index');
