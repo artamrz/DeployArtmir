bf366b8de154027b12f8935a83bd29ec5cb7978a
Arta <arta.mirzadeh@gmail.com>
2025-03-03 12:51:46 -0800

    footer changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,18 +24,22 @@
      </div>
    </div>
  </div>
- <div class="container-fluid mh-100 bg-dark d-flex justify-content-around" style="height: 280px;">
-      <div class="row">
-          <div class="col-sm-4 mt-5 d-grid gap-2">
-              <h3 class="text-light text-center m-0">No Need To Subscribe</h3>
-              <div class="text-light text-center">Newsletters are out dated. Lets get engage in more interactive ways</div>
-              <a href="/contact" class="btn btn-light m-0" style="height:50px;">I Want To Know More</a>
-          </div>    
-          <div class="col-sm-4">
-              <img src="{{ asset('img/newsletter.png')}}" style="margin-top: -75px;" width="500" height="380" alt=""/>
-          </div>  
-      </div>
- </div>
+<div class="container-fluid bg-dark d-flex justify-content-center" style="height: 280px;">
+    <div class="row w-100 justify-content-around align-items-center">
+        <!-- Left Column: Text Content -->
+        <div class="col-sm-4 text-center mb-3 mb-sm-0">
+            <h3 class="text-light">No Need To Subscribe</h3>
+            <p class="text-light">Newsletters are outdated. Let's engage in more interactive ways.</p>
+            <a href="/contact" class="btn btn-light" style="height: 50px; width: 100%;">I Want To Know More</a>
+        </div>
+
+        <!-- Right Column: Image -->
+        <div class="col-sm-4 text-center">
+            <img src="{{ asset('img/newsletter.png') }}" class="img-fluid" style="max-width: 100%; height: auto;" alt="Newsletter Image">
+        </div>
+    </div>
+</div>
+
 
  <div class="container">
     <div class="p-5 mb-4 bg-light rounded-3">
