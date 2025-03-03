ad9d730b0e27c880eeb878ea88664d4a094f5522
Arta <arta.mirzadeh@gmail.com>
2025-03-02 17:35:29 -0800

    posts pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,7 +28,7 @@
       <div class="col-sm-4 mt-5 d-grid gap-2">
         <h3 class="text-light text-center m-0">No Need To Subscribe</h3>
         <div class="text-light text-center">Newsletters are out dated. Lets get engage in more interactive ways</div>
-        <a href="{{ route('pages.contact')}}" class="btn btn-light m-0" style="height:50px;">I Want To Know More</button>
+        <a href="/contact" class="btn btn-light m-0" style="height:50px;">I Want To Know More</button>
       </a>
       <div class="col-sm-4">
         <img src="{{ asset('img/newsletter.png')}}" style="margin-top: -75px;" width="500" height="380" alt=""/>
